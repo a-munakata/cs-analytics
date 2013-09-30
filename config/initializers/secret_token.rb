1193654f13eb7c49e6011d86f8fb3b6705133ac5
a-munakata <a.munakata@4digit.jp>
2013-09-30 19:06:45 +0900

    rails new cs-analytics

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CsAnalytics::Application.config.secret_token = 'f95686c190a386febe53e756a84ebdd36b722c69ddf6980a0c22cd17261f63da4ed9685c21b0cb871a6ff6126eed4cf331ac0429bb351e8ea6644490cb302a53'
